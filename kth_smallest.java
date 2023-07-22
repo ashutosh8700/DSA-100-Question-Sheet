cd8ea289a34f992df256238e44f558ed58bca06f
Ashutosh Dangi <ashutoshdangirdps@gmail.com>
2023-07-22 22:17:42 +0530

    Kth Smallest Element

---
+++ kth_smallest.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class kth_smallest {
+    static class Solution{
+    public static int k_smallest(int[] arr, int l, int r, int k) 
+    { 
+        //Your code here
+        Arrays.sort(arr);
+        int n = arr.length;
+        int result =  arr[k-1];
+        
+        return result;
+    } 
+}
+
+}
